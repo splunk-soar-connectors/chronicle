ec305c7ca43db32e3b8e937e43fbcc2d6f9bc7fe
Dhvani Vora <72184257+dvora-crest@users.noreply.github.com>
2021-12-20 18:15:37 +0530

    Chronicle: Fixed pipeline failures (#4)
    * Chronicle: Fixed pipeline failures
    
    * Chronicle: Added verify flag

--- chronicle_connector.py
+++ chronicle_connector.py
@@ -86,6 +86,7 @@ class ChronicleConnector(BaseConnector):
         self._last_run_hash_digests = dict()
         # Ingestion time dictionary initialization
         self._time_dict = dict()
+        self._verify = False
 
     def _process_empty_response(self, response, action_result):
         """Process empty response.
@@ -2776,7 +2777,7 @@ class ChronicleConnector(BaseConnector):
         url = f'{self.get_phantom_base_url()}rest/container?_filter_name__contains="{name}"&sort=start_time&order=desc'
 
         try:
-            r = requests.get(url, verify=False)
+            r = requests.get(url, verify=self._verify)  # nosemgrep: python.requests.best-practice.use-timeout.use-timeout
         except Exception as e:
             self.debug_print("Error making local rest call: {0}".format(str(e)))
             self.debug_print('DB QUERY: {}'.format(url))
@@ -3401,6 +3402,7 @@ class ChronicleConnector(BaseConnector):
 def main():
     """Use this method to debug action using input test JSON file."""
     import argparse
+    import sys
 
     import pudb
 
@@ -3411,12 +3413,14 @@ def main():
     argparser.add_argument('input_test_json', help='Input Test JSON file')
     argparser.add_argument('-u', '--username', help='username', required=False)
     argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', help='verify', required=False, default=False)
 
     args = argparser.parse_args()
     session_id = None
 
     username = args.username
     password = args.password
+    verify = args.verify
 
     if username is not None and password is None:
 
@@ -3429,7 +3433,7 @@ def main():
             login_url = ChronicleConnector._get_phantom_base_url() + '/login'
 
             print("Accessing the Login page")
-            r = requests.get(login_url, verify=False)
+            r = requests.get(login_url, verify=verify)  # nosemgrep: python.requests.best-practice.use-timeout.use-timeout
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -3442,11 +3446,14 @@ def main():
             headers['Referer'] = login_url
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            r2 = requests.post(login_url,  # nosemgrep: python.requests.best-practice.use-timeout.use-timeout
+                               verify=verify,
+                               data=data,
+                               headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -3463,7 +3470,7 @@ def main():
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
